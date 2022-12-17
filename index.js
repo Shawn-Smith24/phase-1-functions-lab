e3a5b018e37122f2bb5e2f65a4a6d023a10c78e6
shawn <shawnsmith0024@gmail.com>
2022-12-16 20:18:48 -0500

    commit tree 9029f20975b68974d2105e95fe654e25c6ec9760

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+    if (distance < 42){
+        return 42 - distance;
+    }
+    else{
+        return distance - 42
+    }
+}
+
+
+function distanceFromHqInFeet(distance, destination){
+    const feetTraveled = distance * destination
+    return feetTraveled;
+}
